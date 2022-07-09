fdb7b65b40b55826e7aaf233ae17921e47f8c5d0
Ärtiom Yeshchenko <eshchenko@gmail.com>
2022-07-09 11:12:08 +0400

    Allure

--- src/test/java/pages/homepage/HomePageTest.java
+++ src/test/java/pages/homepage/HomePageTest.java
@@ -1,7 +1,6 @@
 package pages.homepage;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
@@ -9,10 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
